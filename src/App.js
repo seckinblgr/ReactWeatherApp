ca5dce392e5f1485edfe3576297a092d9ac14a38
Seçkin <secoblgr52@gmail.com>
2023-03-09 17:43:16 +0300

    first push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios"
+import {useEffect , useState} from "react"
+import City from "./City"
+
 
 function App() {
+  const KEY = "4aed6dda1f0d60d793a922be00a7cc23"
+  const [search,setSearch] = useState("istanbul")
+  const [city, setCity] = useState()
+
+  useEffect(() => {
+    async function getWeather() {
+      try {
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${KEY}&units=metric`);
+        console.log(response)
+        setCity(response.data)
+      } catch (error) {
+        console.error(error);
+      }
+    }getWeather()
+  }, [search])
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex justify-center h-screen min-w-[400px] bg-gradient-to-r from-[#74EBD5] to-[#9FACE6]">
+      <div className="main mt-8 ">
+        <input 
+        type="text" 
+        placeholder="Search City"
+        onKeyDown={(e) => setSearch(e.target.value)} 
+        className="outline-lime-300 text-2xl p-6 font-md text-center bg-gradient-to-r from-[#D3CCE3] to-[#E9E4F0] opacity-60 tracking-widest  h-[40px] rounded-lg border-solid border-2  border-yellow-100 ml-2" />
+        {city && <City city={city} />} 
+      </div>
     </div>
   );
 }
---
+++ src/City.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const City = ({city}) => {
+  return (
+    <div className=' flex-wrap justify-center   w-full h-[600px] mt-[50px] '>
+        <div className=' flex justify-around text-center  flex-col'>
+        <div className='text-2xl p-6 font-md tracking-widest '>
+        <p className='text-zinc-500'>TEMPATURE </p><br />
+            {Math.floor(city.main.temp)}
+        </div>
+        <div className='text-2xl p-6 font-md tracking-widest'>
+        <p className='text-zinc-500'>CITY </p><br />
+            {city.name}
+        </div>
+        <div className='text-2xl p-6 font-md tracking-widest'>
+           <p className='text-zinc-500'>WIND SPEED </p><br />
+           {city.wind.speed}
+        </div>
+        </div>
+   </div>
+  )
+}
+
+export default City
